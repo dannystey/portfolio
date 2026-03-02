ac866b6e39ff6cdf9ed976b5079ada0801e3842e
Danny Stey <mail@danny-stey.de>
2026-03-02 17:55:08 +0100

    set custom server and preview ports, patched build script, and updated deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,8 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+	server: { port: 3300 },
+	preview: { port: 3300 }
+});
