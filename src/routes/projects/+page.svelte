a79a8a49765158e33b252e1901f0246c84a66ab4
Danny Stey <mail@danny-stey.de>
2026-03-09 10:21:06 +0100

    Update project description layout; enhance readability and styling

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -9,11 +9,13 @@
 
     <div class="w-full max-w-4xl mx-auto px-4 absolute top-40 left-1/2 -translate-x-1/2 z-10">
         <h1 class="text-4xl font-black mb-12 text-center">Welcome to Project City</h1>
-        <p class="text-lg mb-12 text-center text-gray-500">
-            This is project city. Every building represents a paid project I have worked on. The height of the building corresponds to the actual hours spent on the project. Every floor represents 20 hours. but I have limited the building height to 10 floors.
-        </p>
+
     </div>
 
     <ProjectCity projects={data.projects}> </ProjectCity>
 </div>
-
+<div class="pb-20 max-w-4xl mx-auto px-5 lg:px-0 space-y-8">
+    <p class="text-center text-neutral-600">
+        This is project city. Every building represents a paid project I have worked on. The height of the building corresponds to the actual hours spent on the project. Every floor represents 20 hours. but I have limited the building height to 10 floors.
+    </p>
+</div>
